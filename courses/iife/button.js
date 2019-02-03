4c1219392e86189abe4525da63babc2a61aa62ce
Maria Fernandez <mariatfj@gmail.com>
2019-02-03 19:21:02 -0400

    Add Editor Config and changes from it

--- courses/iife/button.js
+++ courses/iife/button.js
@@ -3,14 +3,14 @@
 const button = document.getElementById('button');
 
 button.addEventListener('click', (function(){
-	let count = 0;
+    let count = 0;
 
-	return function(){
-		count += 1;
-		debugger
-		if (count === 2) {
-			alert('This alerts appears every other press!');
-			count = 0;
-		}
-	}
+    return function(){
+       count += 1;
+       debugger
+       if (count === 2) {
+         alert('This alerts appears every other press!');
+         count = 0;
+       }
+    }
 })());
