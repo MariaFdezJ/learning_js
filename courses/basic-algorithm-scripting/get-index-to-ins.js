eddb104a4753a1145d5620f071698eeed47246a4
MariaFdezJ <mariafernandez@intellisys.com.do>
2019-03-11 11:07:44 -0400

    Add where do I belong (get index to insert) script

---
+++ courses/basic-algorithm-scripting/get-index-to-ins.js
@@ -0,0 +1,14 @@
+
+function getIndexToIns(arr, num) {
+
+    return arr.reduce( (acc, item)  => {
+
+    	if(item < num) {
+    		acc++;
+    	}
+
+    	return acc;
+    }, 0);
+}
+
+getIndexToIns([40, 60], 50);
