1da74cd39a76a1de120830dc3b7db72324c396cd
MariaFdezJ <mariafernandez@intellisys.com.do>
2019-03-15 15:38:56 -0400

    Removing assignment, and returning value in factorialize - from feedback

--- courses/basic-algorithm-scripting/factorialize-number.js
+++ courses/basic-algorithm-scripting/factorialize-number.js
@@ -2,16 +2,14 @@
 function factorialize(num) {
 
     if(num === 0){
-        num = 1;
-    } else {
+        return 1;
+    } 
 
-        for (let i = num - 1; i > 0; i--){
-            num *= i;
-        }
-
+    for (let i = num - 1; i > 0; i--){
+        num *= i;
     }
 
-    return num;
+	return num;
 }
 
 factorialize(5);
